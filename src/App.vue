a00bb88ae6e061b8a0a38d7ca1182f2e40a59827
um1ng <um1ng@macOS.local>
2017-04-12 15:28:57 +0800

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+<div id="app">
+  <header>
+    <div class="container clear">
+      <a href="" class="logo">
+        <img src="./assets/logo.jpg" alt="Boiling Lab">
+      </a>
+      <nav class="small_nav">
+        <ul>
+          <li class="">
+
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <router-view></router-view>
+</div>
 </template>
 
 <script>
@@ -12,12 +25,15 @@ export default {
 </script>
 
 <style>
+body {
+  background: #201D1E;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
